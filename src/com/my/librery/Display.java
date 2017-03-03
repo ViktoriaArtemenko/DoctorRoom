ab7af689f69e0bbc32908037e8ff92c29c28df51
ViktoriaArtemenko <videothebestmusically@gmail.com>
2017-03-03 19:36:22 +0200

    DoctorRoom
    DoctorRoom

---
+++ src/com/my/librery/Display.java
@@ -0,0 +1,23 @@
+package com.my.librery;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Display implements Observer {
+
+    private Room room;
+
+    public Display(Room room) {
+        this.room = room;
+        room.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        synchronized (room) {
+            System.out.println("Room №" + room.getNumber() + ":  Doctors: " + room.getDoctorCount()
+                    + " | Visitors: " + room.getVisitorCount());
+        }
+    }
+}
+
